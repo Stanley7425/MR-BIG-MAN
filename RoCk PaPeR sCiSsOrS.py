8fc9f81bc52975040c05be6f91263e16f8959a25
Daniel <106247186+DanThePythonMan@users.noreply.github.com>
2022-06-15 13:17:55 +0100

    Update RoCk PaPeR sCiSsOrS.py

--- RoCk PaPeR sCiSsOrS.py
+++ RoCk PaPeR sCiSsOrS.py
@@ -2,5 +2,13 @@ import random
 a=0
 while a<1:
     person=input("Rock paper or scissors?")
-    random.randomint
+    b = random.randint(1,3)
+    if b == 1:
+        c=1
+    elif b == 2:
+        c=2
+    elif b == 3:
+        c=3
+    else:
+        print("Error - line 12)
     
